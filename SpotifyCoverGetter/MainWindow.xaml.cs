d597d411b8d90859741adb4d2f51bedd6fca04de
Cédric Moutet <cedric.moutet@lyceeastier.com>
2023-03-10 02:09:15 +0100

    add the getting image method and add style

--- SpotifyCoverGetter/MainWindow.xaml.cs
+++ SpotifyCoverGetter/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace SpotifyCoverGetter
 {
@@ -27,8 +31,51 @@ namespace SpotifyCoverGetter
 
         private void getCover(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Please, enter a spotify URL.\n\n\nExample : https://open.spotify.com/track/...", "Error", MessageBoxButton.OK, MessageBoxImage.Information);            
-            
+            string data = null;
+            if (link.Text != "")
+            {
+                String enter_link = link.Text;
+                string prefix_link = "https://open.spotify.com/oembed?url=";
+                string full_link = prefix_link + enter_link;
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(full_link);
+                    request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Stream receiveStream = response.GetResponseStream();
+                        StreamReader readStream = null;
+                        if (response.CharacterSet == null)
+                            readStream = new StreamReader(receiveStream);
+                        else
+                            readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                        data = readStream.ReadToEnd();
+                        response.Close();
+                        readStream.Close();
+                    }
+                    //Serialize the JSON
+                    Model modelJson = JsonSerializer.Deserialize<Model>(data);
+
+                    //Manage the Width and Height label
+                    widthLabel.Content = "Width : " + modelJson.width;
+                    heightLabel.Content = "Height : " + modelJson.height;
+
+                    //Manage the Image
+                    Uri imgLink = new Uri(modelJson.thumbnail_url);
+                    image_box.Source = new BitmapImage(imgLink);
+
+                }catch(WebException error)
+                {
+                    MessageBox.Show("Please, enter a valid spotify url.\n\n\nExample: https://open.spotify.com/track/...", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please, enter a spotify URL.\n\n\nExample : https://open.spotify.com/track/...", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            }
         }
-    }
 }
---
+++ SpotifyCoverGetter/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotifyCoverGetter
+{
+    class Model
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string thumbnail_url { get; set; }
+        
+    }
+}
